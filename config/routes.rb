e2664038f54455c2db741838b33571e8840a7011
HououinKyouma-2036 <lixing_guo@ucsb.edu>
2024-11-04 13:55:11 -0800

    fix layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
-  delete "logout", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy", as: :logout
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
