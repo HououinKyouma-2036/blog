312a11949abc95048b0be58723dc0d6d1cd8788b
HououinKyouma-2036 <lixing_guo@ucsb.edu>
2024-11-04 13:55:47 -0800

    Revert "fix layout"
    This reverts commit e2664038f54455c2db741838b33571e8840a7011.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy", as: :logout
+  delete "logout", to: "sessions#destroy"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
